105975022e9fff62cc1505f8d9e305d5d3b2fd12
pratyush997 <pratyush997@outlook.com>
2016-02-26 03:16:18 +0530

    Files added back, Branch relocation done, OLD code is in the clouds :D

---
+++ Project1/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Project1::MyForm form;
+	Application::Run(%form);
+}
